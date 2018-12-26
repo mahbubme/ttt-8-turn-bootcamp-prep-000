882e3f1bd0fc86dfc23e900b8656b21e4cf8f582
Mahbubur Rahman <mail@mahbub.me>
2018-12-26 10:41:28 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,8 +14,12 @@ def valid_move?( board, index )
   end
 end
 
-def position_taken(board, index)
-
+def position_taken?( board, index )
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
 end
 
 def move( board, index, player_character="X" )
